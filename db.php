06056b360e89d01eb2a5075b211f53ef27670a8b
PedroDSimoes <113497762+PedroDSimoes@users.noreply.github.com>
2024-07-13 16:37:50 +0200

    Update db.php

--- db.php
+++ db.php
@@ -1,14 +1,24 @@
 <?php
-$servername = "localhost";
-$username = "root";
-$password = "";
-$dbname = "game_db";
+$url = getenv('DATABASE_URL');
+if ($url) {
+    $dbparts = parse_url($url);
+
+    $hostname = $dbparts['host'];
+    $username = $dbparts['user'];
+    $password = $dbparts['pass'];
+    $database = ltrim($dbparts['path'],'/');
+} else {
+    // Fallback to local database settings for development
+    $hostname = 'localhost';
+    $username = 'root';
+    $password = '';
+    $database = 'game_db';
+}
 
 try {
-    $conn = new PDO("mysql:host=$servername;dbname=$dbname", $username, $password);
+    $conn = new PDO("pgsql:host=$hostname;dbname=$database", $username, $password);
     $conn->setAttribute(PDO::ATTR_ERRMODE, PDO::ERRMODE_EXCEPTION);
 } catch(PDOException $e) {
     echo "Connection failed: " . $e->getMessage();
 }
 ?>
-
